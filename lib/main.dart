17d8a22565a8f648447b052362ca0fc5ac7fad37
Molina Kevin <molinakevinalejandro@gmail.com>
2023-04-21 17:10:06 +0200

    add Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+          () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyApp()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/somos_splash.png'),
+      ),
+    );
+  }
+}
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      ),
+  );
 }
 
 class MyApp extends StatelessWidget {
